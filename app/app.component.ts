593cbd41aae4ce7a0ca29a0faf71330db08edbc9
JamesWebDev <jameswebdev365@gmail.com>
2020-05-09 15:16:03 -0400

    4 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular 5';
-  message = 'Hello World2?'
-  myInput = 'something'
+  
 }
--- app/camera/camera.component.ts
+++ app/camera/camera.component.ts
@@ -6,22 +6,73 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./camera.component.css']
 })
 export class CameraComponent implements OnInit {
-  @ViewChild('video') video;
+  @ViewChild('localVideo') localVideo;
+  @ViewChild('remoteVideo') remoteVideo;
   blur: boolean;
   sepia: boolean;
   invert: boolean;
   flip: boolean;
+  isOfferer: boolean = false;
+  peerConnection: any;
+  configuration: any;
+  localVideoElement: HTMLVideoElement;
+  remoteVideoElement: HTMLVideoElement;
 
+  message = 'MyChatRoom1'
   constructor() { }
 
   ngOnInit() {
-    const videoElement: HTMLVideoElement = this.video.nativeElement;
+    this.localVideoElement = this.localVideo.nativeElement;
+    this.remoteVideoElement = this.remoteVideo.nativeElement;
+
+    this.configuration = {
+      iceServers: [{
+        urls: 'stun:stun.l.google.com:19302' // Google's public STUN server
+      }]
+    };
+  }
+
+  onJoinClicked(){
+    console.log('onJoinClicked called.')
+    this.startWebRtc();
+  }
+
+  startWebRtc(){
+    this.peerConnection = new RTCPeerConnection(this.configuration)
+    this.peerConnection.onicecandidate = event => {
+      console.log('onicecandidate');
+      console.log(event)
+    }
+    if(this.isOfferer){
+      this.peerConnection.onnegotiationneeded = () => {
+        this.peerConnection.createOffer().then(this.onSuccess).catch(this.onError);
+      }
+    }
+    // When a remote stream arrives display it in the #remoteVideo element
+    this.peerConnection.onaddstream = event => {
+      console.log('onaddstream');
+      this.remoteVideoElement.srcObject = event.stream;
+    };    
 
     navigator.mediaDevices.getUserMedia({
-      video: { facingMode: 'user' }
+      video: { facingMode: 'user' },
+      audio: true
     }).then(stream => {
-      videoElement.srcObject = stream;
+      this.localVideoElement.srcObject = stream;
+      this.peerConnection.addStream(stream);
     });
+  }
+
+  onSuccess(){
+    console.log('onSuccess!')
+    this.peerConnection.setLocalDescription(
+      this.message,
+      ()=>{console.log('--1--')},
+      this.onError
+    );
+  }
+  onError(error){
+    console.log('error',error)
   }
 
   getStyles(){
